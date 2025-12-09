b0dcf2e595adeed82d59e6c86f08c927084de475
alt-n1tro <anon@c9cfeccac440.com>
2025-12-09 18:05:08 -0500

    Initial commit.

---
+++ target/debug/deps/rockfish-49686b207730a27b.d
@@ -0,0 +1,5 @@
+/Users/hacker/program_in_rust/rockfish/target/debug/deps/rockfish-49686b207730a27b.d: src/main.rs
+
+/Users/hacker/program_in_rust/rockfish/target/debug/deps/librockfish-49686b207730a27b.rmeta: src/main.rs
+
+src/main.rs:
