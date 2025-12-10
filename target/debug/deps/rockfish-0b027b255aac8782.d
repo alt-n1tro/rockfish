5321bf8f903dc487859384d87780c129d8b48af6
alt-n1tro <anon@c9cfeccac440.com>
2025-12-09 22:57:42 -0500

    Removed build artifacts since I forgot .gitignore

--- target/debug/deps/rockfish-0b027b255aac8782.d
+++
@@ -1,5 +0,0 @@
-/Users/hacker/program_in_rust/rockfish/target/debug/deps/rockfish-0b027b255aac8782.d: src/main.rs
-
-/Users/hacker/program_in_rust/rockfish/target/debug/deps/rockfish-0b027b255aac8782: src/main.rs
-
-src/main.rs:
--- target/debug/rockfish.d
+++
@@ -1,1 +0,0 @@
-/Users/hacker/program_in_rust/rockfish/target/debug/rockfish: /Users/hacker/program_in_rust/rockfish/src/main.rs
